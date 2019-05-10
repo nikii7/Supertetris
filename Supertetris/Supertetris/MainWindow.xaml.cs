008867755c1c109e890c5765b966fa0274fe6b39
Niklas Höllinger <niklas.hoellinger@outlook.de>
2019-05-10 09:49:25 +0200

    Commi Design

--- Supertetris/Supertetris/MainWindow.xaml.cs
+++ Supertetris/Supertetris/MainWindow.xaml.cs
@@ -25,17 +25,16 @@ namespace Supertetris
         {
             InitializeComponent();
 
-            dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
-            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
-            dispatcherTimer.Start();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
-       
-        
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
--- Supertetris/Supertetris/Spielfeld.cs
+++ Supertetris/Supertetris/Spielfeld.cs
@@ -15,7 +15,7 @@ namespace Supertetris
 
         public void ZeilenEntfernen()
         {
-            -
+            
         }
     }
 }
