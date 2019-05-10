b5f9f8a1a2b938c64e5ad67f8b594a00ab010bb8
Thomas Neumayer <neumayer.t02@htl-ottakring.ac.at>
2019-05-10 08:44:32 +0200

    Klasse für den Highscore erstellt

---
+++ Supertetris/Supertetris/Highscore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Supertetris
+{
+    class Highscore
+    {
+        public static void Main()
+        {
+            string path = @"c:\temp\MyTest.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("And");
+                    sw.WriteLine("Welcome");
+                }
+            }
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
--- Supertetris/Supertetris/MainWindow.xaml.cs
+++ Supertetris/Supertetris/MainWindow.xaml.cs
@@ -24,10 +24,6 @@ namespace Supertetris
         {
             InitializeComponent();
 
-            dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
-            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
-            dispatcherTimer.Start();
         }
     }
 }
